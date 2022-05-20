3f14a2efaf9e348b5ff2d2457390c510450040ff
IskrenP <100073655+IskrenP@users.noreply.github.com>
2022-05-20 13:55:55 +0300

    Lecture 2

---
+++ Lecture2/Multiplication Table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Lekciq1 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int number = Integer.parseInt(scan.nextLine());
+            for (byte i = 1; i <= 10; i++)
+            {
+                System.out.printf("%d X %d = %d%n", number, i, number * i);
+            }
+        }
+    }
