ed7c28327a0e801fc62a02cd9c9b277a339f71ae
IskrenP <100073655+IskrenP@users.noreply.github.com>
2022-06-13 19:32:03 +0300

    Lecture 12

---
+++ Lecture12/Top Integers.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ArraysTopIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+        int[] arr = new int[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+            for (int i = 0; i < arr.length; i++) {
+                boolean isBigger = true;  
+                for (int j = i+1; j<arr.length; j++){
+                if (arr[i] <= arr[j]) {
+                    isBigger = false;
+                }
+            }
+            if (isBigger) {
+                System.out.print(arr[i] + " ");
+            }
+
+        }
+
+
+    }
+}
